12f73f9ca3700980ee6096a2e32086ef4f2523a8
sue445 <sue445@sue445.net>
2026-01-03 17:51:27 +0900

    Add test files

---
+++ test/versions.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+
+      # c.f. https://github.com/hashicorp/terraform-provider-google/blob/master/CHANGELOG.md
+      version = "7.14.1"
+    }
+    google-beta = {
+      source = "hashicorp/google-beta"
+      # c.f. https://github.com/hashicorp/terraform-provider-google-beta/blob/master/CHANGELOG.md
+      version = "7.14.1"
+    }
+  }
+  required_version = ">= 1.0"
+}
