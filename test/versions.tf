d00c798d37ac7cf550ce481eb973f5b09cb3811a
sue445 <sue445@sue445.net>
2026-01-04 09:17:48 +0900

    :golf:

--- test/versions.tf
+++ test/versions.tf
@@ -1,14 +1,11 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
-
-      # c.f. https://github.com/hashicorp/terraform-provider-google/blob/master/CHANGELOG.md
+      source  = "hashicorp/google"
       version = "7.14.1"
     }
     google-beta = {
-      source = "hashicorp/google-beta"
-      # c.f. https://github.com/hashicorp/terraform-provider-google-beta/blob/master/CHANGELOG.md
+      source  = "hashicorp/google-beta"
       version = "7.14.1"
     }
   }
